aa912d1e9cec55efc03421e12c587656f6c5eee4
Qendresa Hajdari <qh43141@ubt-uni.net>
2025-02-15 14:26:47 +0100

    Update user email

--- user.test.js
+++ user.test.js
@@ -19,9 +19,9 @@ describe('User Management Tests', () => {
 
     test('Need to update user email', () => {
         userManager.registerUser("Qendresa Hajdari", "qendresa@example.com", "user1");
-        userManager.updateUserEmail("user1", "qendresa2@example.com");
+        userManager.updateUserEmail("user1", "qendresahajdari@example.com");
 
-        expect(userManager.getUserById("user1").email).toBe("qendresa2@example.com");
+        expect(userManager.getUserById("user1").email).toBe("qendresahajdari@example.com");
     });
 
     test('Return null if this user does not exist', () => {
