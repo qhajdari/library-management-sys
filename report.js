a980504e2f5b98fc9970f1ed9ab444d4b5e2ff92
Qendresa Hajdari <qh43141@ubt-uni.net>
2025-02-15 16:23:56 +0100

    greed - test passed

--- report.js
+++ report.js
@@ -1,3 +1,16 @@
-class ReportSystem{
-    
+class ReportSystem {
+    constructor(library, borrowSystem) {
+        this.library = library;
+        this.borrowSystem = borrowSystem;
+    }
+
+    listAllBooks() {
+        return Object.values(this.library.books);
+    }
+
+    listAllBorrowedBooks() {
+        return this.borrowSystem.borrowedBooks;
+    }
 }
+
+module.exports = ReportSystem;
