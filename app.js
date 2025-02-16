fcba18f678d24229f22df6e816784acdeb1612cb
Qendresa Hajdari <qh43141@ubt-uni.net>
2025-02-16 17:59:36 +0100

    Added check books and borrowed books

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const Library = require('./library'); // Import Library Class
 const UserManager = require('./user'); // Import User Class
+const BorrowSystem = require('./borrow'); // Import Borrow Class
+
 
 const library = new Library(); // Create a library instance
 const userManager = new UserManager();
+const borrowSystem = new BorrowSystem(library, userManager);
 
 
 // Add new book
@@ -16,9 +19,9 @@ library.updateCopies("123456", 5);
 console.log(library.getBookByISBN("123456")); // Return copies to 5
 
 // Delete the book
-library.deleteBook("123456");
+//library.deleteBook("123456");
 // Print deleted book by ISBN
-console.log(library.getBookByISBN("123456")); // Return null
+//console.log(library.getBookByISBN("123456")); // Return null
 
 
 //GetUserById
@@ -29,7 +32,13 @@ console.log(userManager.getUserById("user1"));
 userManager.updateUserEmail("user1", "qendresahajdari@exaple.com", "user1");
 console.log(userManager.getUserById("user1"));
 
-//
+//Check if the book exists
+console.log("Book before borrowing:", library.getBookByISBN("123456"));
+
+//Borrowed books by user
+borrowSystem.borrowBook("user1", "123456");
+console.log("Borrowed books:", borrowSystem.getBorrowedBooks("user1"));
+
 
 
 
